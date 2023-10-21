914e6c01a8daee892d0fade4e93e77fb0b8fea11
AndreyCorpse28090 <280990corpse@gmail.com>
2023-10-21 20:15:57 +0300

    class

--- File_Manager_Bukov/File_Manager_Bukov.cpp
+++ File_Manager_Bukov/File_Manager_Bukov.cpp
@@ -1,6 +1,34 @@
 ﻿#include <iostream>
 
+class File
+{
+private:
+    std::string NameFile; // имя файла
+    std::string extension; // расширение файла
+    int FileSize;  // размер файла
+    std::string PathFile; // путь к файлу
+    std::string ContentsFile; // Содержимое файла(текстового)
+
+};
+
+class Folder
+{
+private:
+    std::string NameFolder; // имя папки
+    std::string PathFolder; // путь к папке
+    std::string ListFolder; //Список файлов и подпапок
+
+};
+
+class Disk
+{
+private:
+    std::string DiskName; // Имя диска
+    std::string LiskDisk; // Содержимое диска
+};
+
 int main()
 {
+    setlocale(LC_ALL,"rus");
     std::cout << "Hello World!\n";
 }
