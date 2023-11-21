207fad40e2b4be86b72d6e148f51da320e8f009d
AndreyCorpse28090 <280990corpse@gmail.com>
2023-11-21 21:49:04 +0300

    поиск по маске, доделать

--- File_Manager_Bukov/File_Manager_Bukov.cpp
+++ File_Manager_Bukov/File_Manager_Bukov.cpp
@@ -2,9 +2,12 @@
 #include <string>
 #include <filesystem>
 #include <fstream>
+#include <regex>
+
 
 namespace fs = std::filesystem;
 
+// Абстрактный базовый класс для файловой системы
 class FileSystemObject
 {
 public:
@@ -12,6 +15,7 @@ public:
     virtual ~FileSystemObject() = default;
 };
 
+// Класс для представления файла
 class File : public FileSystemObject
 {
 private:
@@ -26,6 +30,7 @@ public:
         std::cout << "File: " << name << " at " << path << std::endl;
     }
 
+    // Метод для создания файла
     void create()
     {
         std::ofstream file(path + "/" + name);
@@ -34,6 +39,7 @@ public:
     }
 };
 
+// Класс для представления папки
 class Folder : public FileSystemObject
 {
 private:
@@ -48,6 +54,7 @@ public:
         std::cout << "Folder: " << name << " at " << path << std::endl;
     }
 
+    // Метод для создания папки
     void create()
     {
         fs::create_directory(path + "/" + name);
@@ -55,13 +62,15 @@ public:
     }
 };
 
+// Класс для управления файловой системой
 class FileManager
 {
 private:
     std::string currentPath;
 
 public:
-    void showContents(bool showSizes = true)
+    // Метод для отображения содержимого директории
+    void showContents(bool showSizes = true, const std::string& mask = "")
     {
         try
         {
@@ -73,28 +82,32 @@ public:
                 {
                     auto entryPath = entry.path();
 
-                    try
+                    // Добавлен фильтр по маске
+                    if (mask.empty() || std::regex_match(entryPath.filename().u8string(), std::regex(mask)))
                     {
-                        std::string entryType = fs::is_directory(entryPath) ? "Папка" : "Файл";
-                        std::cout << entryType << ": " << entryPath.filename().u8string();
-
-                        if (showSizes)
+                        try
                         {
-                            if (fs::is_regular_file(entryPath))
-                            {
-                                std::cout << " (Размер: " << fs::file_size(entryPath) << " байт)";
-                            }
-                            else if (fs::is_directory(entryPath))
-                            {
-                                std::cout << " (Размер: " << calculateFolderSize(entryPath) << " байт)";
-                            }
-                        }
+                            std::string entryType = fs::is_directory(entryPath) ? "Папка" : "Файл";
+                            std::cout << entryType << ": " << entryPath.filename().u8string();
 
-                        std::cout << std::endl;
-                    }
-                    catch (const std::exception& e)
-                    {
-                        std::cerr << "Ошибка при обработке файла/папки: " << e.what() << std::endl;
+                            if (showSizes)
+                            {
+                                if (fs::is_regular_file(entryPath))
+                                {
+                                    std::cout << " (Размер: " << fs::file_size(entryPath) << " байт)";
+                                }
+                                else if (fs::is_directory(entryPath))
+                                {
+                                    std::cout << " (Размер: " << calculateFolderSize(entryPath) << " байт)";
+                                }
+                            }
+
+                            std::cout << std::endl;
+                        }
+                        catch (const std::exception& e)
+                        {
+                            std::cerr << "Ошибка при обработке файла/папки: " << e.what() << std::endl;
+                        }
                     }
                 }
             }
@@ -117,17 +130,20 @@ public:
         }
     }
 
+    // Метод для создания папки
     void createFolder(const std::string& name)
     {
         fs::create_directory(currentPath + "/" + name);
     }
 
+    // Метод для создания файла
     void createFile(const std::string& name)
     {
         std::ofstream file(currentPath + "/" + name);
         file.close();
     }
 
+    // Метод для удаления объекта (файла или папки)
     void deleteObject(const std::string& name)
     {
         try
@@ -158,6 +174,7 @@ public:
         }
     }
 
+    // Метод для переименования объекта (файла или папки)
     void rename(const std::string& oldName, const std::string& newName)
     {
         try
@@ -181,6 +198,7 @@ public:
         }
     }
 
+    // Метод для чтения текстового файла
     void readTextFile(const std::string& fileName)
     {
         std::ifstream file(currentPath + "/" + fileName);
@@ -200,16 +218,19 @@ public:
         }
     }
 
+    // Метод для установки текущего пути
     void setCurrentPath(const std::string& path)
     {
         currentPath = path;
     }
 
+    // Метод для получения текущего пути
     std::string getCurrentPath() const
     {
         return currentPath;
     }
 
+    // Метод для перехода в директорию
     void navigateToDirectory(const std::string& directoryName)
     {
         std::string newPath = currentPath + "/" + directoryName;
@@ -224,7 +245,7 @@ public:
         }
     }
 
-
+    // Метод для перехода на уровень выше
     void navigateUp()
     {
         std::string parentPath = fs::path(currentPath).parent_path().u8string();
@@ -239,7 +260,7 @@ public:
         }
     }
 
-private:
+    // Метод для вычисления размера папки
     uintmax_t calculateFolderSize(const fs::path& folderPath)
     {
         uintmax_t size = 0;
@@ -252,6 +273,53 @@ private:
         }
         return size;
     }
+
+
+    // поиск по маске
+    void searchByMask(const std::string& mask)
+    {
+        try
+        {
+            fs::path currentPathObj(currentPath);
+
+            // Используем lexically_normal() для обработки символов маски
+            fs::path searchPath = currentPathObj.lexically_normal();
+
+            // Перебираем файлы в директории
+            bool found = false;
+            for (const auto& entry : fs::directory_iterator(searchPath))
+            {
+                std::string entryName = entry.path().filename().u8string();
+
+                // Сравниваем расширение файла с маской
+                if (entry.path().extension() == mask)
+                {
+                    found = true;
+                    std::cout << "Найден объект с маской " << mask << ": " << entryName << std::endl;
+                }
+            }
+
+            if (!found)
+            {
+                std::cout << "Объекты с маской " << mask << " не найдены в директории " << currentPath << ".\n";
+            }
+        }
+        catch (const std::filesystem::filesystem_error& e)
+        {
+            std::cerr << "Ошибка работы с файловой системой: " << e.what() << std::endl;
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << "Необработанное исключение: " << e.what() << std::endl;
+        }
+        catch (...)
+        {
+            std::cerr << "Необработанное неизвестное исключение.\n";
+        }
+    }
+
+
+
 };
 
 int main()
@@ -278,6 +346,7 @@ int main()
             << "7. Просмотреть содержимое текстового файла\n"
             << "8. Перейти в директорию\n"
             << "9. Вернуться в предыдущую директорию\n"
+            << "A. Поиск по маске\n"  // Добавлена новая операция для поиска по маске
             << "0. Выход\n"
             << "Текущая директория: " << fileManager.getCurrentPath() << std::endl;
         std::cin >> choice;
@@ -343,6 +412,14 @@ int main()
         case '9':
             fileManager.navigateUp();
             break;
+        case 'A':
+        {
+            std::string mask;
+            std::cout << "Введите маску для поиска: ";
+            std::cin >> mask;
+            fileManager.searchByMask(mask);
+            break;
+        }
         case '0':
             std::cout << "Выход из программы.\n";
             break;
